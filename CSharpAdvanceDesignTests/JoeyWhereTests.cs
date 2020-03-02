7d4b80b81e6e98b4d9d926ad29d7c8dfdfb78ef4
Peter <sheraium@gmail.com>
2020-03-02 22:08:56 +0800

    extract parameter with condition as Func<Product, bool>

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
@@ -24,7 +25,7 @@ namespace CSharpAdvanceDesignTests
                 new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
             };
 
-            var actual = JoeyWhere(products);
+            var actual = JoeyWhere(products, current => current.Price > 200 && current.Price < 500);
 
             var expected = new List<Product>
             {
@@ -36,13 +37,37 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<Product> JoeyWhere(IEnumerable<Product> products)
+        [Test]
+        public void find_products_that_price_between_200_and_500_and_cost_less_than_30()
+        {
+            var products = new List<Product>
+            {
+                new Product {Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e"},
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+                new Product {Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e"},
+                new Product {Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e"},
+                new Product {Id = 5, Cost = 51, Price = 510, Supplier = "Momo"},
+                new Product {Id = 6, Cost = 61, Price = 610, Supplier = "Momo"},
+                new Product {Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo"},
+                new Product {Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo"}
+            };
+
+            var actual = JoeyWhere(products, current => current.Price > 200 && current.Price < 500 && current.Cost<30);
+
+            var expected = new List<Product>
+            {
+                new Product {Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+        private IEnumerable<Product> JoeyWhere(IEnumerable<Product> products, Func<Product, bool> predicate)
         {
             var enumerator = products.GetEnumerator();
             while (enumerator.MoveNext())
             {
                 var current = enumerator.Current;
-                if (current.Price > 200 && current.Price < 500)
+                if (predicate(current))
                 {
                     yield return current;
                 }
